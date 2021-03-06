138999799c8e532dc2f797d03bcc43c70cfd9830
saurabhlog9 <78251669+saurabhlog9@users.noreply.github.com>
2021-03-06 12:55:49 +0530

    Initial Commit

---
+++ mySite/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class item(models.Model):
+    title=models.CharField(max_length=100)
+class orderItem(models.Model):
+    pass
+
+class order(models.Model):
+    pass
